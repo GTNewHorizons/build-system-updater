fcd60a3244f25ff00bb09a3e9e4a4f98fb078d0b
Raven Szewczyk <git@eigenraven.me>
2023-01-31 09:10:13 +0000

    Add guards to check for RFG

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,13 +4,24 @@ set -eu
 main() {
   repository_url=$(git remote get-url origin)
   repository_name="$(basename "$repository_url" .git)"
-  
+
   echo "--> Updating build system of $repository_name"
-  
+
+  if ! grep -Eq "GTNewHorizons/ExampleMod1\\.7\\.10/(main|master)/build\\.gradle" build.gradle; then
+    echo "$repository_name is not using the ExampleMod buildscript"
+    exit 1
+  fi
+
+  if ! grep -Fq "com.gtnewhorizons.retrofuturagradle" build.gradle; then
+    echo "$repository_name is not using the RFG buildscript"
+    exit 1
+  fi
+
   chmod +x gradlew
+  git add --chmod=+x gradlew # Fix file permissions in git permanently
   ./gradlew updateBuildScript
   ./gradlew clean build test
-  
+
   echo "<-- Build system updated successfully"
 }
 
